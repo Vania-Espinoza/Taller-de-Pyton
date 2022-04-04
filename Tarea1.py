9c50408d6a826fe68c770ab48bb9390c6f77ce8c
rainefpp <70712013+rainefpp@users.noreply.github.com>
2022-04-03 21:41:15 -0400

    numero de lineas
    primer avance

--- Tarea1.py
+++ Tarea1.py
@@ -1,2 +1,20 @@
 #Tarea 1
 
+f = open("El_Caos_Reptante.txt","rt",encoding="utf-8")
+
+Numero_Lineas=0
+Numero_Palabras_Total=0
+Numero_Palabras_No_Repetidas=0
+Numero_Caracteres_Con_Espacio=0
+Numero_Caracteres_Sin_Espacio=0
+
+for x in f:
+    print(x)
+    if "\n" in x:
+        Numero_Lineas += 1
+    
+
+print("el numero de lineas es: ",Numero_Lineas)
+
+
+
