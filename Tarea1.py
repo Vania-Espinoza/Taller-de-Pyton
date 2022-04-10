b05000ea4ea7e05c95ebe873e78782256e92caff
rainefpp <70712013+rainefpp@users.noreply.github.com>
2022-04-10 17:33:50 -0400

    avance

--- Tarea1.py
+++ Tarea1.py
@@ -1,5 +1,8 @@
 #Tarea 1
 
+#Menu de libros
+from typing import List
+
 
 def menu():
     print ("Listado de libros a utilizar")
@@ -10,18 +13,19 @@ def menu():
     print ("\t5 - Para_Leer_Al_Atardecer")
     print ("\t6 - Una_corta_historia_del_eBook")
     print ("\t7 - Salir")
-
+#Menu de opciones
 def menu2():
     print ("¿Qué desea hacer con el libro seleccionado?")
     print ("\t1 - Conocer su estadistica")
     print ("\t2 - Buscar una palabra y ver cuantas veces se repite")
     print ("\t3 - Remplazar una palabra en el texto")
     print ("\t4 - Salir")
-
+#Declaracion de variables de conteo
 Numero_Lineas = 0
 Repeticiones = 0
 Espacios = 0
 Numero_Caracteres_Con_Espacio=0
+Numero_Palabras=0
 menu()
 opcionMenu = input("Seleccione un número: ")
 print ("\n")
@@ -32,26 +36,33 @@ if opcionMenu=="1":
     opciónMenu2 = input("Escriba el numero: ")
     print ("\n")
     if opciónMenu2=="1":
-        datos = a.read()
-        palabras = datos.split()
-        for x in datos:
+        Lista_Palabras = list()
+        for x in a:
             if "\n" in x:
                 Numero_Lineas += 1
             Espacios += x.count(" ")
+            Numero_Palabras += len(x.split())
+            Lista_Palabras += x.split()
+            print(Lista_Palabras)
             Numero_Caracteres_Con_Espacio += len(x)
-            Numero_Caracteres_Sin_Espacio = Numero_Caracteres_Con_Espacio - Espacios
+        for aux in range(len(Lista_Palabras)):
+            Palabra_comparar=Lista_Palabras[aux]
+            var = Lista_Palabras.count(Palabra_comparar)
+            if var > 1:
+                Repeticiones += 1
+        Numero_Caracteres_Con_Espacio -= Numero_Lineas
+        Numero_Caracteres_Sin_Espacio = Numero_Caracteres_Con_Espacio - Espacios
+        Numero_Palabras_No_Repetidas = Numero_Palabras - Repeticiones
         print("La estadística del libro seleccionado es la siguiente")
         print("El numero de lineas es: ",Numero_Lineas)
-        print("El numero total de palabras es: ", len(palabras))
-        print("El numero de palabras no repetidas es: ", )
+        print("El numero total de palabras es: ", Numero_Palabras)
+        print("El numero de palabras no repetidas es: ", Numero_Palabras_No_Repetidas)
         print("El numero de caracteres con espacios es: ", Numero_Caracteres_Con_Espacio)
         print("El numero de caracteres sin espacios es: ", Numero_Caracteres_Sin_Espacio)
 
     elif opciónMenu2=="2":
         Palabra_Buscada = input("Ingrese la palabra a buscar: ")
-        datos = a.read()
-        palabras = datos.split()
-        for x in datos:
+        for x in a:
             y = x.lower() 
             Palabra_Buscada = Palabra_Buscada.lower()
             Repeticiones += y.count(Palabra_Buscada)
